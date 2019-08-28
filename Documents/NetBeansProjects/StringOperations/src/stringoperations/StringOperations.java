237ce936a78c29b0d6f070f31ad695a166363e67
0608011776 <0608011776@2221-176-04.browardschools.local>
2019-08-28 11:16:26 -0400

    initial code

---
+++ Documents/NetBeansProjects/StringOperations/src/stringoperations/StringOperations.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringoperations;
+
+import java.util.Scanner;
+/**
+ *
+ * @author 0608011776
+ */
+public class StringOperations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*String str1="Hello";
+        String str2="Michael";
+        
+        String str3 = "You are " + str2;
+        
+        System.out.println (str1.compareTo(str2));
+        */
+        boolean boolVal;
+        byte byteVal;
+        char charValue;
+        
+        Scanner myInput = new Scanner(System.in);
+        System.out.print("please enter a boolean value:  ");
+        boolVal = myInput.nextBoolean();
+        
+        System.out.print("please enter a byte value:  ");
+        byteVal = myInput.nextByte();
+        
+        
+      
+      
+                
+                
+    }
+    
+}
